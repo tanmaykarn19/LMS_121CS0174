ec31387145e1d4df9523c9611df4ca19799ab650
Tanmay Karn <tanmaykarn58@gmail.com>
2024-05-10 20:30:30 +0530

    auth and category commit

---
+++ LMS/user_login.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, login, logout
+
+
+def REGISTER(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # check username:
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username already exists!')
+            return redirect('register')
+
+        #check email:
+        if User.objects.filter(email = email).exists():
+            messages.warning(request, 'Email already exists!')
+            return redirect('register')
+
+
+
+        user = User(
+            username = username,
+            email = email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+    return render(request, 'registration/register.html')
+
+
+def DO_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = EmailBackEnd.authenticate(request, username=email, password=password)
+
+        if user != None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email and password are invalid')
+            return redirect('login')
+
+
+def PROFILE(request):
+    return render(request, 'registration/profile.html')
+
+
+def PROFILE_UPDATE(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_id = request.user.id
+
+        user = User.objects.get(id=user_id)
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+
+        if password!= None and password != "":
+            user.set_password(password)
+        user.save()
+
+        messages.success(request, 'profile updated successfully!')
+        return redirect('profile')
---
+++ LMS/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import redirect, render
+from app.models import Categories, Course
+
+def BASE(request):
+    return render(request, 'base.html')
+
+
+def HOME(request):
+    category = Categories.objects.all().order_by('id')[0:5]
+    course = Course.objects.filter(status = 'PUBLISH').order_by('-id')
+
+
+
+    context = {
+        'category' : category,
+        'course' : course,
+    }
+    return render (request, 'Main/home.html', context)
+
+
+def SINGLE_COURSE(request):
+    return render(request, 'Main/single_course.html')
+
+
+def CONTACT_US(request):
+    return render(request, 'Main/contact_us.html')
+
+
+def ABOUT_US(request):
+    return render(request, 'Main/about_us.html')
